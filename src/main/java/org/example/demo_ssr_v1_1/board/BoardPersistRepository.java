2d98464f932c5f75cfa5f12070577f105e694394
kimgsun <gsune@kakao.com>
2025-12-10 10:00:50 +0900

    20251210 mustache

---
+++ src/main/java/org/example/demo_ssr_v1_1/board/BoardPersistRepository.java
@@ -0,0 +1,64 @@
+package org.example.demo_ssr_v1_1.board;
+
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+// DB -- CRUD
+@RequiredArgsConstructor
+@Repository // IoC
+public class BoardPersistRepository {
+
+    // DI
+    private final EntityManager entityManager;
+
+    @Transactional
+    public Board save(Board board) {
+        // 엔티티 매니저 자동으로 insert 쿼리 만들어 던진다.
+        entityManager.persist(board);
+        return board;
+    }
+
+    // 게시글 전체 조회
+    public List<Board> findAll() {
+        return entityManager
+                .createQuery("SELECT b FROM Board b ORDER BY b.createdAt DESC")
+                .getResultList();
+    }
+
+    // 게시글 단건 조회
+    public Board findById(Long id) {
+        Board board = entityManager.find(Board.class, id);
+        return board;
+    }
+
+    // 수정
+    @Transactional
+    public Board updateById(Long id, BoardRequest.UpdateDTO req) {
+        Board board = entityManager.find(Board.class, id);
+
+        if (board == null) {
+            throw new IllegalArgumentException("수정할 게시글을 찾을 수 없습니다.");
+        }
+
+        board.setTitle(req.getTitle());
+        board.setContent(req.getContent());
+        board.setUsername(req.getUsername());
+
+        // 더티 체킹
+        // 1. 개발자가 직접 update 쿼리를 작성 안해도 된다
+        // 2. 변경된 필드만 자동으로 Update 가 된다
+        // 3. 영속성 컨텍스트가 엔티티 상태를 자동 관리 한다
+        // 4. 1차 캐시의 엔티티 정보도 자동 갱신
+
+        return board;
+    }
+
+    // 삭제
+}
---
+++ src/main/java/org/example/demo_ssr_v1_1/user/User.java
@@ -0,0 +1,5 @@
+package org.example.demo_ssr_v1_1.user;
+
+// 엔티티 화면 보고 설계해 보세요.
+public class User {
+}
